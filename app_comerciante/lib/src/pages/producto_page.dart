1fa6b9eeef5c69e8a0449d429a08c8583eec5710
carlosdrm1 <carlosdrm@gmail.com>
2020-04-04 21:13:24 -0500

    Create HomePage and ProductoPage with models

---
+++ app_comerciante/lib/src/pages/producto_page.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_provider.dart';
+//import 'package:formvalidation/src/bloc/validators.dart';
+//import 'package:formvalidation/src/utils/utils.dart' as utils ;
+
+class ProductoPage extends StatefulWidget {
+
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+  class _ProductoPageState extends State<ProductoPage> {
+    
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final productoProvider = new ProductosProvider(); 
+    
+
+  ProductoModel producto = new ProductoModel();
+  bool _guardando = false;
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+  final ProductoModel prodData = ModalRoute.of(context).settings.arguments;
+
+
+    if(prodData != null){
+      producto = prodData;
+    }  
+
+
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: Text('Productos'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.photo_size_select_actual), 
+            onPressed: (){},
+            ),
+            IconButton(
+            icon: Icon(Icons.camera_alt), 
+            onPressed: (){},
+            )
+        ]
+      ),
+      body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(15.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: <Widget>[
+                  _crearNombre(),
+                  _crearPrecio(),
+                  _crearDisponible(),
+                  _crearBoton(),
+                ],
+              ),
+            ),
+          ),
+
+      ),
+    );
+  }
+
+  Widget _crearNombre(){
+
+    return TextFormField(
+      initialValue: producto.titulo,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Producto'
+      ),
+      onSaved: (value) => producto.titulo = value,
+      validator: (value){
+        if( value.length < 3){
+          return 'Ingrese el nombre del producto';
+        }else{
+          return null;
+        }
+      },
+    );
+    
+  }
+
+  Widget _crearPrecio(){
+    return TextFormField(
+      initialValue: producto.valor.toString(),
+      keyboardType: TextInputType.numberWithOptions( decimal:true ),
+      decoration: InputDecoration(
+        labelText: 'Precio'
+      ),
+      onSaved: (value) => producto.valor = double.parse (value),
+    );
+  }
+
+
+  Widget _crearCategoria(){
+
+    return TextFormField(
+      initialValue: producto.titulo,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Categoria'
+      ),
+      onSaved: (value) => producto.titulo = value,
+      validator: (value){
+        if( value.length < 3){
+          return 'Ingrese el nombre de la categoria';
+        }else{
+          return null;
+        }
+      },
+    );
+    
+  }
+
+  Widget _crearUbicacion(){
+
+    return TextFormField(
+      initialValue: producto.titulo,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Ubicacion'
+      ),
+      onSaved: (value) => producto.titulo = value,
+      validator: (value){
+        if( value.length < 3){
+          return 'Ingrese el nombre de la categoria';
+        }else{
+          return null;
+        }
+      },
+    );
+    
+  }
+
+  Widget _crearDisponible(){
+
+    return SwitchListTile(
+      value: producto.disponible, 
+      title: Text('Disponible'),
+      activeColor: Colors.deepPurple,
+      onChanged: (value) => setState( (){
+        producto.disponible = value;
+      })
+      );
+  }
+
+  Widget _crearDescripcion(){
+
+    return TextFormField(
+      initialValue: producto.titulo,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Descripcion'
+      ),
+      onSaved: (value) => producto.titulo = value,
+      validator: (value){
+        if( value.length < 3){
+          return 'Ingrese la descripcion del producto';
+        }else{
+          return null;
+        }
+      },
+    );
+    
+  }
+
+  Widget _crearBoton(){
+    
+    return RaisedButton.icon(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0)
+      ),
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      label: Text('Guardar'),
+      icon: Icon(Icons.save),
+      onPressed: ( _guardando ) ? null : _submit,
+      );
+  }
+
+  void _submit(){
+
+      if( !formKey.currentState.validate() ) return;
+
+      formKey.currentState.save();
+      
+      setState(() {_guardando = true;  });
+
+      if( producto != null ){
+        productoProvider.crearProducto(producto);
+      }else{
+        productoProvider.editarProducto(producto);
+      }
+
+      productoProvider.crearProducto(producto); 
+
+      //setState(() {_guardando = false;  });
+      mostrarSnackBar('Registro almacenado');
+
+      Navigator.pop(context);
+  }
+
+  void mostrarSnackBar(String mensaje){
+    
+    final snackbar = SnackBar(
+      content: Text( mensaje ),
+      duration: Duration( milliseconds: 1500 ),
+    );
+
+    scaffoldKey.currentState.showSnackBar(snackbar);
+
+
+  }
+
+}
