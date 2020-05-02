9c87e8218b6c82da2200dd2323f2efdafa909019
Jorge <jorgealeman31@gmail.com>
2020-05-01 20:48:49 -0500

    actualizar el proyecto

---
+++ app_comerciante/lib/src/models/evento_model.dart
@@ -0,0 +1,45 @@
+// To parse this JSON data, do
+//
+//     final client = clientFromJson(jsonString);
+
+import 'dart:convert';
+
+EventoModel eventoModelFromJson(String str) => EventoModel.fromJson(json.decode(str));
+
+String eventoModelToJson(EventoModel data) => json.encode(data.toJson());
+
+class EventoModel {
+    String id;
+    String nombre;
+    String ubicacion;
+    String capacidad;
+    String fotoUrl;
+    bool disponible;
+
+    EventoModel({
+        this.id,
+        this.nombre,
+        this.ubicacion,
+        this.capacidad,
+        this.fotoUrl,
+        this.disponible = true,
+    });
+
+    factory EventoModel.fromJson(Map<String, dynamic> json) => EventoModel(
+        id          : json["id"],
+        nombre      : json["nombre"],
+        ubicacion   : json["ubicacion"],
+        capacidad   : json["capacidad"],
+        fotoUrl     : json["fotoUrl"],
+        disponible  : json["disponible"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "nombre": nombre,
+        "ubicacion": ubicacion,
+        "capacidad": capacidad,
+        "fotoUrl": fotoUrl,
+        "disponible": disponible,
+    };
+}
---
+++ app_comerciante/lib/src/pages/home_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App-Comerciantes'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[          
+          _cardTipo2(context,'eventoList'),
+          SizedBox(height: 30.0),
+           _cardTipo2(context,'productoList'),
+          SizedBox(height: 30.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2(BuildContext context,  String direccionamiento) {
+
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: <Widget>[
+
+          FadeInImage(
+            image: NetworkImage('https://res.cloudinary.com/dlrlog6ij/image/upload/v1588379572/hwvfvmywpjvrawcjbr8f.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration( milliseconds: 200 ),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          ListTile(
+              title: Text('$direccionamiento'),              
+              onTap: () => Navigator.pushNamed(context,'$direccionamiento' ),
+            ),
+        ],
+      ),
+    );
+
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
+      ),
+    );
+
+  }
+
+}
