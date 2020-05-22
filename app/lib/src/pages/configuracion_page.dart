8d98e657a235915d97a5fd4055ab703167331ff4
Jaleman1005 <47544866+Jaleman1005@users.noreply.github.com>
2020-05-22 16:03:29 -0500

    update

--- app/lib/src/pages/configuracion_page.dart
+++ app/lib/src/pages/configuracion_page.dart
@@ -1,3 +1,6 @@
+/* import 'package:app/src/bloc/provider.dart';
+import 'package:app/src/bloc/theme.dart'; */
+
 import 'package:flutter/material.dart';
 
 class ConfiguracionPage extends StatefulWidget {
@@ -10,6 +13,7 @@ class ConfiguracionPage extends StatefulWidget {
 class _ConfiguracionPageState extends State<ConfiguracionPage> {
   @override
   Widget build(BuildContext context) {
+    /* final theme = Provider.of<ThemeChanger>(context); */
     return Scaffold(
         appBar: AppBar(
           title: Text('Configuracion'),
@@ -23,38 +27,42 @@ class _ConfiguracionPageState extends State<ConfiguracionPage> {
   }
 }
 
-// One entry in the multilevel list displayed by this app.
 class Entry {
   Entry(this.title, this.children);
-
   final String title;
   final Column children;
 }
 
-// The entire multilevel list displayed by this app.
 final List<Entry> data = <Entry>[
   Entry(
     'Temas',
     Column(
         children: [
-          Text('perro funciono'),
-          Text('perro funciono'),
-          Text('perro funciono'),
-          Text('perro funciono'),
+          /* FlatButton(
+              onPressed: () => theme.setTheme(ThemeData.light()),
+              child: Text('Light', style: Theme.of(context).textTheme.subtitle1),
+            ),
+            FlatButton(
+              onPressed: () => theme.setTheme(ThemeData.dark()),
+              child: Text('Dark', style: Theme.of(context).textTheme.subtitle1),
+            ),
+            FlatButton(
+              onPressed: () => theme.setTheme(ThemeData(
+                  primaryColor: Colors.cyan,
+                  floatingActionButtonTheme: FloatingActionButtonThemeData(
+                      backgroundColor: Colors.cyan))),
+              child: Text('Cyan', style: Theme.of(context).textTheme.subtitle1),
+            ), */
         ],
     )
   )
 ];
 
-// Displays one Entry. If the entry has children then it's displayed
-// with an ExpansionTile.
 class EntryItem extends StatelessWidget {
   const EntryItem(this.entry);
-
   final Entry entry;
 
   Widget _buildTiles(Entry root) {
-    /* if (root.children.isEmpty) */ /* return ListTile(title: Text(root.title)); */
     return ExpansionTile(
       key: PageStorageKey<Entry>(root),
       title: Text(root.title),
